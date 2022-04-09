5f0e84538d1edc1c17d5d3f7e612b87201aaf08e
PurpleAnteater <I.Kupchinenko@gmail.com>
2022-04-09 12:24:53 +0300

    Created a sptofy playlist service with interface for http client requests and responds

---
+++ SpotifyApi/Interfaces/ISpotifyPlaylistService.cs
@@ -0,0 +1,12 @@
+﻿namespace SpotifyApi.Interfaces
+{
+    public interface ISpotifyPlaylistService
+    {
+        /// <summary>
+        /// Method that returns an information abot requested playlist
+        /// </summary>
+        /// <param name="playlistId"></param>
+        /// <returns></returns>
+        Task GetPlaylist(string playlistId);
+    }
+}
---
+++ SpotifyApi/Services/SpotifyPlaylistService.cs
@@ -0,0 +1,20 @@
+﻿using SpotifyApi.Interfaces;
+
+namespace SpotifyApi.Services
+{
+    public class SpotifyPlaylistService : ISpotifyPlaylistService
+    {
+        private readonly HttpClient _httpClient;
+
+        public SpotifyPlaylistService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task GetPlaylist(string playlistId)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "");
+            var respone = _httpClient.GetAsync(playlistId);
+        }
+    }
+}
