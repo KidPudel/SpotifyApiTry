2ec6188f37a2a75f716b551b86b4763759160423
PurpleAnteater <I.Kupchinenko@gmail.com>
2022-04-18 14:28:35 +0300

    Models for client details to get authorized and authorization token

---
+++ SpotifyApi/Models/OAuthClientModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SpotifyApi.Models
+{
+    /// <summary>
+    /// Details that we need to get authenticated
+    /// </summary>
+    public class OAuthClientModel
+    {
+        [JsonPropertyName("client_id")]
+        public string ClientId { get; set; }
+
+        [JsonPropertyName("client_secret")]
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ SpotifyApi/Models/TokenModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SpotifyApi.Models
+{
+    /// <summary>
+    /// Token details for client credentials
+    /// </summary>
+    public class TokenModel
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonPropertyName("expires_in")]
+        public int? ExpiresIn { get; set; }
+    }
+}
