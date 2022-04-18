4bed7c69453672dd07ebfd55214f67fb4ad1662e
PurpleAnteater <I.Kupchinenko@gmail.com>
2022-04-18 14:30:34 +0300

    Added http typed client services for retrieving access token and displayng info about playlist

--- SpotifyApi/Program.cs
+++ SpotifyApi/Program.cs
@@ -1,25 +1,47 @@
+using SpotifyApi.Interfaces;
+using SpotifyApi.Services;
+using System.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient<IOAuthClientService, OAuthClientService>(httpClient =>
+{
+    httpClient.BaseAddress = new Uri("https://accounts.spotify.com/");
+    httpClient.DefaultRequestHeaders.Accept.Clear();
+    // what we can accept in response
+    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    // set content_type header
+    httpClient.DefaultRequestHeaders.Add("Content_Type", "application/x-www-form-encoded");
+});
+
+builder.Services.AddHttpClient<ISpotifyPlaylistService, SpotifyPlaylistService>(httpClient =>
+{
+    httpClient.BaseAddress = new Uri("https://api.spotify.com/v1/playlists/");
+    httpClient.DefaultRequestHeaders.Accept.Clear();
+    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(name: "default", pattern: "{controller=Spotify}/{action=Hello}");
+    endpoints.MapControllers();
+});
 
 app.Run();
