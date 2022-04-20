1b2b0b8470db374be56f6c5710b4788a24846d3a
PurpleAnteater <I.Kupchinenko@gmail.com>
2022-04-20 13:11:21 +0300

    Controller that get authroazation token, retrieves playlist json file and returning it as json. sdded parameter that comes from form in POST request in main page as application/x-www-form-urlencoded.

---
+++ SpotifyApi/Controllers/SpotifyController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using SpotifyApi.Interfaces;
+using SpotifyApi.Services;
+using SpotifyApi.Models;
+
+namespace SpotifyApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SpotifyController : ControllerBase
+    {
+        private readonly IOAuthClientService _authClientService;
+        private readonly ISpotifyPlaylistService _playlistService;
+        
+        public SpotifyController(IOAuthClientService authClientService, ISpotifyPlaylistService playlistService)
+        {
+            _authClientService = authClientService;
+            _playlistService = playlistService;
+        }
+
+
+        [HttpPost("/playlist_info")]
+        [Consumes("application/x-www-form-urlencoded")]
+        public async Task<IActionResult> PlaylistInfo([FromForm] string playlistId)
+        {
+            TokenModel tokenModel = await _authClientService.GetToken();
+            var playlistInfo = await _playlistService.GetPlaylist(tokenModel.AccessToken, playlistId);
+
+            return Ok(playlistInfo);
+        }
+    }
+}
